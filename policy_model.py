42c37e24263fbcbbc3298dd6963b1eb2c10879fc
cynthiayechen <yc90@rice.edu>
2023-03-16 15:19:20 -0700

    Create policy_model.py
    create policy model with q-learning model only (for the moment ;D planning to add sarsa etc.)

---
+++ policy_model.py
@@ -0,0 +1,55 @@
+import numpy as np
+import pandas as pd
+import csv
+import random
+import matplotlib.pyplot as plt
+from datetime import datetime
+
+
+def q_learning(input_csv, number_of_states, number_of_actions=3, input_discount=0.95, alpha=0.1):
+    state_space = number_of_states
+    action_space = number_of_actions
+    # action value function
+    Q = np.zeros((state_space, action_space))
+
+    with open("data/" + input_csv, "r") as f:
+        reader = csv.reader(f, delimiter="\t")
+        for i, line in enumerate(reader):
+            if i != 0:
+                [temp] = line
+                s, r, a, sp = temp.split(",")
+                s, r, a, sp = int(s), int(r), int(a), int(sp)
+                # Q[s, a] += α * (r + γ * maximum(Q[s′,:]) - Q[s, a])
+                max_val = np.max(Q[sp - 1])
+                Q[s-1, a-1] += alpha * (r + input_discount * max_val - Q[s-1, a-1])
+
+    policy = np.zeros(number_of_states)
+    for i in range(number_of_states):
+        flag_no_actions_rewards = True
+        for j in Q[i]:
+            if j != 0:
+                flag_no_actions_rewards = False
+        # there are some states in the medium and large problems that do not have
+        # actions and rewards associated with them in the data provided
+        if flag_no_actions_rewards:
+            policy[i] = random.randint(0, number_of_actions - 1)
+        else:
+            policy[i] = np.argmax(Q[i])
+
+    return policy
+
+
+if __name__ == '__main__':
+    # number_of_states = 6
+    small_q_learning = q_learning("small.csv", 6)
+
+    with open('data/small_q_learning.policy', 'w') as f:
+        for line in small_q_learning:
+            f.write("%s\n" % str(int(line)))
+
+    # number_of_states = 52
+    big_q_learning = q_learning("big.csv", 52)
+
+    with open('data/big_q_learning.policy', 'w') as f:
+        for line in big_q_learning:
+            f.write("%s\n" % str(int(line)))
